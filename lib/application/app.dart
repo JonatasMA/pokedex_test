3310bf95749f81484c22c3b2e16665ff11af746f
Jonatas Macedo <jonatasmacedo70@hotmail.com>
2021-08-22 10:07:46 -0300

    fix: remove appbar shadow

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        shadowColor: Color.fromRGBO(70, 70, 70, 1),
+        shadowColor: Color.fromRGBO(70, 70, 70, 0),
         backgroundColor: Color.fromRGBO(70, 70, 70, 1),
         title: Text(widget.title),
       ),
