9e59fe499e7ea67f41eedff55c67c6137be0c913
Jonatas Macedo <jonatasmacedo70@hotmail.com>
2021-08-22 16:44:11 -0300

    perf: implements GridView with build

--- lib/application/widgets/pokemons_list.dart
+++ lib/application/widgets/pokemons_list.dart
@@ -1,4 +1,5 @@
 import 'package:pokedex_test/application/models/pokemons.dart';
+import 'package:pokedex_test/application/pages/detail_page.dart';
 import 'package:pokedex_test/application/widgets/list_frame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -8,26 +9,27 @@ class PokemonsList extends StatelessWidget {
 
   PokemonsList(this.pokemons);
 
-  buildFrames() {
-    List<Widget> pokemonsWidgets = [];
-    for (var pokemon in this.pokemons.pokemons) {
-      pokemonsWidgets.add(ListFrame(pokemon.id, pokemon.name, pokemon.types, pokemon.color));
-    }
-    return pokemonsWidgets;
-  }
-
   @override
   Widget build(BuildContext context) {
-    // return Text('aaaa');
     return Container(
       color: Color.fromRGBO(70, 70, 70, 1),
-      child: GridView.count(
-            primary: false,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 2,
-            children: this.buildFrames())
+      child: GridView.builder(
+        itemCount: this.pokemons.pokemons.length,
+        gridDelegate:
+            new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return new Card(
+            child: new InkResponse(
+              child: ListFrame(this.pokemons.pokemons[index]),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        DetailPage(this.pokemons.pokemons[index])));
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
